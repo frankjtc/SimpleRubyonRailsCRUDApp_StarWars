a8858b92c6fa44db8aa61d2727b2435b4069a69d
frankjtc <frank@HAL9000.local>
2014-12-15 23:33:56 -0800

    Second commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141212005619) do
+ActiveRecord::Schema.define(version: 20141216072247) do
+
+  create_table "star_wars", force: true do |t|
+    t.string   "name"
+    t.string   "race"
+    t.string   "republican"
+    t.string   "padawa"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "starwars", force: true do |t|
     t.string   "name"
